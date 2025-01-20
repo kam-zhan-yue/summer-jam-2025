f7f4c12f121cf888f313e7d5dc08da658b898cda
Alex Kam <kamzhanyue@gmail.com>
2025-01-20 14:46:16 +0900

    feat: add menu buttons

--- src/ui/main_menu.rs
+++ src/ui/main_menu.rs
@@ -1,16 +1,28 @@
-use bevy::prelude::*;
+use crate::schedule::GameSet;
+use bevy::{color::palettes::basic::*, prelude::*};
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
 
 #[derive(Component, Debug)]
 pub struct MainMenu;
 
 #[derive(Component, Debug)]
-pub struct PlayButton;
+pub struct SinglePlayerButton;
+
+#[derive(Component, Debug)]
+pub struct TwoPlayerButton;
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_main_menu);
+        app.add_systems(
+            Update,
+            (handle_buttons, handle_single_player_button).in_set(GameSet::Ui),
+        );
     }
 }
 
@@ -33,24 +45,114 @@ pub fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
         .with_children(|parent| {
             // Title
             parent
-                .spawn(
-                    (Node {
-                        flex_direction: FlexDirection::Row,
-                        justify_content: JustifyContent::Center,
-                        ..default()
-                    }),
-                )
+                .spawn(Node {
+                    flex_direction: FlexDirection::Row,
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                })
                 .with_children(|parent| {
                     // Text
                     parent.spawn((
-                        Text::new("Text Example"),
+                        Text::new("Swirlie, Wedgie, Willie!"),
                         TextFont {
                             font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                            font_size: 25.0,
+                            font_size: 50.0,
                             ..default()
                         },
                         Label,
                     ));
                 });
+            // Single Player Button
+            parent
+                .spawn((
+                    SinglePlayerButton,
+                    Button,
+                    Node {
+                        width: Val::Px(150.0),
+                        height: Val::Px(40.0),
+                        border: UiRect::all(Val::Px(1.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    BorderColor(Color::BLACK),
+                    BorderRadius::all(Val::Px(5.0)),
+                    BackgroundColor(NORMAL_BUTTON),
+                ))
+                .with_child((
+                    Text::new("1 Player"),
+                    TextFont {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 22.0,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                ));
+            // Two Player Button
+            parent
+                .spawn((
+                    TwoPlayerButton,
+                    Button,
+                    Node {
+                        width: Val::Px(150.0),
+                        height: Val::Px(40.0),
+                        border: UiRect::all(Val::Px(1.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    BorderColor(Color::BLACK),
+                    BorderRadius::all(Val::Px(5.0)),
+                    BackgroundColor(NORMAL_BUTTON),
+                ))
+                .with_child((
+                    Text::new("2 Players"),
+                    TextFont {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 22.0,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                ));
         });
+}
+
+fn handle_buttons(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, &mut BorderColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut colour, mut border_colour) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                *colour = PRESSED_BUTTON.into();
+                border_colour.0 = RED.into();
+            }
+            Interaction::Hovered => {
+                *colour = HOVERED_BUTTON.into();
+                border_colour.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                *colour = NORMAL_BUTTON.into();
+                border_colour.0 = Color::BLACK;
+            }
+        }
+    }
+}
+
+fn handle_single_player_button(
+    interaction_query: Query<&Interaction, (Changed<Interaction>, With<SinglePlayerButton>)>,
+) {
+    let Ok(interaction) = interaction_query.get_single() else {
+        return;
+    };
+
+    if *interaction == Interaction::Pressed {
+        println!("Single Player");
+    }
 }
